f4e5cd48ba9259dd0c58865da19c0dab7c53e8de
Kriston Costa <kriston@Kristons-MacBook-Pro.local>
2021-03-12 11:22:40 -0500

    Simplifying mesh generation

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,265 +1,113 @@
 use crate::ecs::component::*;
 use crate::mesh::MeshVertex;
 
+const CUBE_COORDINATES: [[f32; 3]; 8] = [
+    [-0.5, -0.5, 0.5],
+    [0.5, -0.5, 0.5],
+    [0.5, -0.5, -0.5],
+    [-0.5, -0.5, -0.5],
+    [-0.5, 0.5, 0.5],
+    [0.5, 0.5, 0.5],
+    [0.5, 0.5, -0.5],
+    [-0.5, 0.5, -0.5],
+];
+
+const UVS: [[f32; 2]; 4] = [
+    [0.125, 1.0 - 0.9375],
+    [0.1875, 1.0 - 0.9375],
+    [0.125, 1.0 - 1.0],
+    [0.1875, 1.0 - 1.0],
+];
+
+enum CubeSide {
+    Top,
+    Bottom,
+    Left,
+    Right,
+    Forward,
+    Backward,
+}
+
 pub fn make_block() -> MeshReference {
     let mut vertex_data: Vec<MeshVertex> = Vec::new();
     let mut index_data: Vec<u32> = Vec::new();
 
-    let uvs = [
-        [0.125, 1.0 - 0.9375],
-        [0.1875, 1.0 - 0.9375],
-        [0.125, 1.0 - 1.0],
-        [0.1875, 1.0 - 1.0],
-    ];
-
-    let p0 = [-0.5, -0.5, 0.5];
-    let p1 = [0.5, -0.5, 0.5];
-    let p2 = [0.5, -0.5, -0.5];
-    let p3 = [-0.5, -0.5, -0.5];
-    let p4 = [-0.5, 0.5, 0.5];
-    let p5 = [0.5, 0.5, 0.5];
-    let p6 = [0.5, 0.5, -0.5];
-    let p7 = [-0.5, 0.5, -0.5];
-
-    let mut offset = 0;
+    let offset = 0;
 
     // TOP
 
-    vertex_data.push(MeshVertex {
-        position: p7.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
+    let (verticies, indices, offset) = generate_quad(CubeSide::Top, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
-    vertex_data.push(MeshVertex {
-        position: p6.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
+    let (verticies, indices, offset) = generate_quad(CubeSide::Bottom, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
-    vertex_data.push(MeshVertex {
-        position: p5.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
+    let (verticies, indices, offset) = generate_quad(CubeSide::Left, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
-    vertex_data.push(MeshVertex {
-        position: p4.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
+    let (verticies, indices, offset) = generate_quad(CubeSide::Right, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
+    let (verticies, indices, offset) = generate_quad(CubeSide::Forward, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
-    // Bottom
-
-    vertex_data.push(MeshVertex {
-        position: p0.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p1.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p2.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p3.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
-
-    // Left
-
-    vertex_data.push(MeshVertex {
-        position: p7.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p4.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p0.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p3.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
-
-    // RIGHT
-
-    vertex_data.push(MeshVertex {
-        position: p5.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p6.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p2.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p1.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
-
-    // Forward
-
-    vertex_data.push(MeshVertex {
-        position: p4.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p5.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p1.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p0.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
-
-    // back
-
-    vertex_data.push(MeshVertex {
-        position: p6.clone(),
-        tex_coords: uvs[3].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p7.clone(),
-        tex_coords: uvs[2].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p3.clone(),
-        tex_coords: uvs[0].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    vertex_data.push(MeshVertex {
-        position: p2.clone(),
-        tex_coords: uvs[1].clone(),
-        normal: [0.0, 1.0, 0.0],
-    });
-
-    index_data.extend(
-        [
-            3 + offset,
-            1 + offset,
-            0 + offset,
-            3 + offset,
-            2 + offset,
-            1 + offset,
-        ]
-        .iter(),
-    );
-    offset += 4;
+    let (verticies, indices, _) = generate_quad(CubeSide::Backward, offset);
+    vertex_data.extend(verticies.iter());
+    index_data.extend(indices.iter());
 
     MeshReference {
         idx: 0,
         vertex_data: vertex_data.into_boxed_slice(),
         index_data: index_data.into_boxed_slice(),
     }
+}
+
+fn generate_quad(side: CubeSide, index_offset: u32) -> ([MeshVertex; 4], [u32; 6], u32) {
+    let vertex_idx = match side {
+        CubeSide::Top => [7, 6, 5, 4],
+        CubeSide::Bottom => [0, 1, 2, 3],
+        CubeSide::Left => [7, 4, 0, 3],
+        CubeSide::Right => [5, 6, 2, 1],
+        CubeSide::Forward => [4, 5, 1, 0],
+        CubeSide::Backward => [6, 7, 3, 2],
+    };
+
+    (
+        [
+            MeshVertex {
+                position: CUBE_COORDINATES[vertex_idx[0]].clone(),
+                tex_coords: UVS[3].clone(),
+                normal: [0.0, 1.0, 0.0],
+            },
+            MeshVertex {
+                position: CUBE_COORDINATES[vertex_idx[1]].clone(),
+                tex_coords: UVS[2].clone(),
+                normal: [0.0, 1.0, 0.0],
+            },
+            MeshVertex {
+                position: CUBE_COORDINATES[vertex_idx[2]].clone(),
+                tex_coords: UVS[0].clone(),
+                normal: [0.0, 1.0, 0.0],
+            },
+            MeshVertex {
+                position: CUBE_COORDINATES[vertex_idx[3]].clone(),
+                tex_coords: UVS[1].clone(),
+                normal: [0.0, 1.0, 0.0],
+            },
+        ],
+        [
+            3 + index_offset,
+            1 + index_offset,
+            0 + index_offset,
+            3 + index_offset,
+            2 + index_offset,
+            1 + index_offset,
+        ],
+        index_offset + 4,
+    )
 }
